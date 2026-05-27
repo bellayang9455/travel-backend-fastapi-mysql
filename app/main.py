2df182c0cad178f65de17cbfcbdf747bb7214690
yyy <mbellayang@gmail.com>
2026-05-27 23:20:21 +0800

    fix request import

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from .config import settings
 from .database import Base, engine, SessionLocal
