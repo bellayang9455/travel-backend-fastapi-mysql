664b9d0261b336650ff395a5fd08038ce1ec6802
yyy <mbellayang@gmail.com>
2026-05-27 23:25:40 +0800

    fix bcrypt 72 bytes

--- app/security.py
+++ app/security.py
@@ -1,28 +1,39 @@
-from datetime import datetime, timedelta
-from typing import Optional
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
 from jose import jwt
-
 from .config import settings
-
-
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
+def safe_password(password: str) -> str:
+    # bcrypt 最大 72 bytes
+    return password.encode("utf-8")[:72].decode("utf-8", errors="ignore")
+
+
+def get_password_hash(password: str):
+    password = safe_password(password)
+    return pwd_context.hash(password)
+
+
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password.encode("utf-8")[:72], hashed_password)
+    plain_password = safe_password(plain_password)
+    return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password):
-    return pwd_context.hash(password.encode("utf-8")[:72])
 
-# 3. 產生 JWT Token (通行證)
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
-    
+
+    expire = datetime.utcnow() + timedelta(
+        minutes=settings.access_token_expire_minutes
+    )
+
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.secret_key,
+        algorithm=settings.algorithm
+    )
+
     return encoded_jwt
